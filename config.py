a306204f603cce8073483d2b859a2781a44dcf62
Neo <89941741+MatrixBytes@users.noreply.github.com>
2022-05-28 00:38:49 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+class errors:
+    unrecognized_variable_type  = "Error! Unknow variable type.\nAviable variable types: int for integer, str for string, lst for list and flt for float."
+    bad_var_content             = "Error! Invalid content for variable type %"
+    var_asigned_with_other_type = "Error! This variable has the type %"
+    unknown_method              = "Error! Unknown method for the command %" 
+    unknown_lib                 = "Error! Unknown library %"
+    function_needs_code         = "Error! Expected an block after function asignment"
+    return_out_of_function      = "Error! Used the return statement out of function"
+
+class imports:
+    preimports = ['list', 'iostream', 'string']
